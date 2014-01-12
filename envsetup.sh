750f59a9136eacda66f89d543bf81a096eb41d2b
Chester Kener <Cl3Kener@gmail.com>
2014-01-11 20:17:47 -0500

    Linaro by default
    Signed-off-by: Chester Kener <Cl3Kener@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -153,7 +153,7 @@ function setpaths()
     case $ARCH in
         x86) toolchaindir=x86/i686-linux-android-$targetgccversion/bin
             ;;
-        arm) toolchaindir=arm/arm-linux-androideabi-$targetgccversion/bin
+        arm) toolchaindir=arm/LINARO/bin
             ;;
         mips) toolchaindir=mips/mipsel-linux-android-$targetgccversion/bin
             ;;
